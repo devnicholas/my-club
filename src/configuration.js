5ad2d25ebe5c456008291f62f6db7d21a04977f8
Nicholas Stefano <nicholas.stefanob@gmail.com>
2021-09-30 20:22:53 -0300

    fix configuration link and club infors

--- src/configuration.js
+++ src/configuration.js
@@ -2,8 +2,8 @@ const utils = require("./utils");
 
 const configs = utils.storage.getItemLocal("config");
 const form = document.getElementById("configurations");
-form.elements["clubLong"].value = configs.clubLong;
-form.elements["club"].value = configs.club;
+form.elements["clubLong"].value = configs.clubLong || '';
+form.elements["club"].value = configs.club || '';
 utils.layout.addClickEvent("saveConfigs", () => {
 	const club = form.elements["club"].value;
 	const clubLong = form.elements["clubLong"].value;
